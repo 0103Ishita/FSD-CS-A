9dfc1c42ed8e29dfa4378f07b5c11a3957783c9d
Ishita <ishitabanshiwal@gmail.com>
2026-02-17 15:33:13 +0530

    crud

--- server/server.js
+++ server/server.js
@@ -1,43 +1,47 @@
 import http from "http";
 import os from "os";
+
 const port = 4001;
-let body = "";
-const data = [];
 const server = http.createServer((req, res) => {
   const url = req.url;
-  if (url == "/" && req.method == "GET") {
-    res.end("<h1>Home Page </h1>");
-  } else if (url == "/about" && req.method == "GET") {
-    res.end("About page");
-  } else if (url == "/contact" && req.method == "GET") {
-    res.end("Contact Page ");
-  } else if (url == "/system" && req.method == "GET") {
+  const method = req.method;
+
+  if (url == "/" && method == "GET") {
+    res.write("Home page");
+  } else if (url == "/contact" && method == "GET") {
+    res.write("Contact page");
+  } else if (url == "/system" && method == "GET") {
+    //informatiion of our system
+
+    //since this is object but sysdata is string format so we have to convert into string
+    //thios is done through JSON.stringify() method
     const sysdata = {
-      platform: os.platform(),
-      arch: os.arch(),
-      cpu: os.cpus().length,
-      totalmem: (os.totalmem() / 1024 ** 3).toFixed(2)+"GB",
-      freemem: (os.freemem() / 1024 ** 3).toFixed(2)+"GB",
+      operatingSystem: os.platform(),
+      architecture: os.arch(),
+      cpuLength: os.cpus().length,
+      TotalMemory: (os.totalmem() / 1024 ** 3).toFixed(2),
+      freeMemory: (os.freemem() / 1024 ** 3).toFixed(2),
+      network: os.networkInterfaces(),
     };
-    res.setHeader("content-Type", "application/json");
-    res.end(JSON.stringify(sysdata));
-  } else if (url == "/senddata" && req.method == "POST") {
-    req.on("data", (chunk) => {
-      body = body + chunk;
-    });
-    req.on("end", () => {
-      res.statusCode = 201;
-      console.log(body, "datasend");
-      data.push(body);
-      res.end(JSON.stringify(data));
-    });
-  } else if (url == "/viewdata" && req.method == "GET") {
-    res.end(body);
+    res.write(JSON.stringify(sysdata));
+  } else if (url == "/users" && method == "GET") {
+    res.write("List of Users page");
+  } else if (url == "/createuser" && method == "POST") {
+    res.write("Create User page");
+  } else if (url.startsWith("/users") && method == "GET") {
+    res.write("Search user");
+  } else if (url.startsWith("/users") && method == "PUT") {
+    res.write("Update user");
+  } else if (url.startsWith("/users") && method == "DELETE") {
+    res.write("Delete user");
   } else {
-    res.statusCode = 404;
-    res.end("Error Page");
+    res.write("error page");
   }
+  res.end();
 });
+
+
+//bind the function and then execute the server
 server.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
